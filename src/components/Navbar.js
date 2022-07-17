94ef64e2de9897f46b9713fdb8fdb1bf72efeebf
arubyo <alyssaofriel@gmail.com>
2022-07-17 20:09:20 +1000

    add buttons and styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,33 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { Button, ButtonContainer } from './NavbarStyles';
 
 function Navigation() {
   return (
     <>
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Shortly</Navbar.Brand>
+          <Navbar.Brand href="#home">
+          <h3 style={{ color: "black",
+				textAlign: "left",
+                fontSize: "26px",
+                fontWeight: "bolder"
+				 }}>
+            Shortly
+            </h3></Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Features</Nav.Link>
             <Nav.Link href="#features">Pricing</Nav.Link>
             <Nav.Link href="#pricing">Resources</Nav.Link>
+           
+       
           </Nav>
+          <ButtonContainer>
+            <Nav.Link href="#pricing">Login</Nav.Link>
+            <Button>Sign Up</Button>
+
+            </ButtonContainer>
         </Container>
       </Navbar>
     </>
---
+++ src/components/NavbarStyles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+<style> @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap');</style>
+
+export const Button = styled.button`
+border-radius: 18px;
+color: #FFF;
+font-weight: bold;
+background-color:  hsl(180, 66%, 49%); 
+border: none;
+padding: 5px 15px;
+`;
+
+
+
+export const ButtonContainer = styled.div`
+display: flex;
+align-content: flex-end;
+justify-content: flex-end;
+
+
+`;
